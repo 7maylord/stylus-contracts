19e00ca8a9232d1f62e1900bb477fd24fb6f3762
olumide <olumideadenigba@gmail.com>
2025-08-25 09:17:08 +0100

    chore: add erc20, strings, and erc6909

---
+++ stylus-erc20/src/lib.rs
@@ -0,0 +1,61 @@
+// Only run this as a WASM if the export-abi feature is not set.
+#![cfg_attr(not(any(feature = "export-abi", test)), no_main)]
+extern crate alloc;
+
+// Modules and imports
+pub mod erc20;
+
+use alloy_primitives::{Address, U256};
+use stylus_sdk::{
+    msg,
+    prelude::*
+};
+use crate::erc20::{Erc20, Erc20Params, Erc20Error};
+
+/// Immutable definitions
+struct StylusTokenParams;
+impl Erc20Params for StylusTokenParams {
+    const NAME: &'static str = "rampop";
+    const SYMBOL: &'static str = "POP";
+    const DECIMALS: u8 = 18;
+}
+
+// Define the entrypoint as a Solidity storage object. The sol_storage! macro
+// will generate Rust-equivalent structs with all fields mapped to Solidity-equivalent
+// storage slots and types.
+sol_storage! {
+    #[entrypoint]
+    struct StylusToken {
+        // Allows erc20 to access StylusToken's storage and make calls
+        #[borrow]
+        Erc20<StylusTokenParams> erc20;
+    }
+}
+
+#[public]
+#[inherit(Erc20<StylusTokenParams>)]
+impl StylusToken {
+    /// Initialize contract with initial supply to deployer
+    pub fn init(&mut self, initial_supply: U256) -> Result<(), Erc20Error> {
+        self.erc20.mint(msg::sender(), initial_supply)?;
+        Ok(())
+    }
+
+    /// Mints tokens
+    pub fn mint(&mut self, value: U256) -> Result<(), Erc20Error> {
+        self.erc20.mint(msg::sender(), value)?;
+        Ok(())
+    }
+
+    /// Mints tokens to another address
+    pub fn mint_to(&mut self, to: Address, value: U256) -> Result<(), Erc20Error> {
+        self.erc20.mint(to, value)?;
+        Ok(())
+    }
+
+    /// Burns tokens
+    pub fn burn(&mut self, value: U256) -> Result<(), Erc20Error> {
+        self.erc20.burn(msg::sender(), value)?;
+        Ok(())
+    }
+}
